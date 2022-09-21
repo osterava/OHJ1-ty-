971809b802d1739f1c7026b79af4cb810131d80d
Terävä Olli <olli.s.terava@student.jyu.fi>
2022-09-21 16:22:09 +0300

    1. vaiheen toteutusta alustavasti

---
+++ Harjoitustyö/KauppaPeli/KauppaPeli.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+namespace KauppaPeli;
+
+public class KauppaPeli : PhysicsGame
+{
+    public override void Begin()
+    {
+        Level.Background.Color = Color.Black;
+        Level.CreateBorders();
+        for (int i = 0; i < 10; i++)
+        {
+            LuoSatunainenYmpyra(this, Level.Left, Level.Bottom, Level.Right, 0);
+
+        }
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+
+    }
+   public static void LuoSatunainenYmpyra(PhysicsGame peli,double pieninx,double pieniny,double isoinx,double isoinY)
+    {
+        double leveys = RandomGen.NextDouble(40, 40);
+        double korkeus = RandomGen.NextDouble(40, 40);  
+        PhysicsObject ympyra = new PhysicsObject(leveys, korkeus, Shape.Circle);
+        ympyra.Position = RandomGen.NextVector(pieninx, pieniny, isoinx, isoinY);
+        peli.Add(ympyra);
+    }
+}
+
+
