129e38469ba32e71029a5b4c9369e995cda9daff
Terävä Olli <olli.s.terava@student.jyu.fi>
2022-10-06 22:24:01 +0300

    lisätty pelaajan kuva, sekä pelaaja-objekti

--- Harjoitustyö/KauppaPeli/KauppaPeli.cs
+++ Harjoitustyö/KauppaPeli/KauppaPeli.cs
@@ -11,24 +11,39 @@ public class KauppaPeli : PhysicsGame
 {
     public override void Begin()
     {
-        Level.Background.Color = Color.Black;
-        Level.CreateBorders();
-        for (int i = 0; i < 50; i++) // vihaiset asiakkat
+        BoundingRectangle alaosa = new BoundingRectangle(new Vector(Level.Left, 0), Level.BoundingRect.BottomRight);
+        BoundingRectangle ylaosa = new BoundingRectangle(Level.BoundingRect.TopLeft, new Vector(Level.Right, 0));
+
+        for (int i = 0; i < 15; i++) // vihaiset asiakkat
         {
-            LuoSatunainenYmpyra(this, Level.Left, Level.Bottom, Level.Right, 0);
+            LuoSatunaisetAsiakkaat(this, alaosa,60);
 
         }
+        PhysicsObject pelaaja =  LuoSatunaisetAsiakkaat(this,ylaosa,50);
+        pelaaja.Image = LoadImage("pelaaja");
+        pelaaja.Color = Color.Red;
+
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        Level.Background.Color = Color.Black;
+        Level.CreateBorders();
 
     }
-   public static void LuoSatunainenYmpyra(PhysicsGame peli,double pieninX,double pieninY,double isoinX,double isoinY)
+    /// <summary>
+    /// Aliohjelma luo vihamieliset asiakkaat
+    /// </summary>
+    /// <param name="peli">mihin peliin ympyrä luodaan</param>
+    /// <param name="umpyra">minkä ymppyrän sisälle luodaan eli paikka</param>
+    /// <param name="vauhti">kolmion vauhti alussa</param>
+    public static PhysicsObject LuoSatunaisetAsiakkaat(PhysicsGame peli,BoundingRectangle umpyra,double vauhti)
     {
         double leveys = RandomGen.NextDouble(40, 40);
         double korkeus = RandomGen.NextDouble(40, 40);  
+        Vector liike = RandomGen.NextVector(0, vauhti);
         PhysicsObject ympyra = new PhysicsObject(leveys, korkeus, Shape.Circle);
-        ympyra.Position = RandomGen.NextVector(pieninX, pieninY, isoinX, isoinY);
+        ympyra.Hit(liike);
         peli.Add(ympyra);
+        return ympyra;
     }
 }
 
