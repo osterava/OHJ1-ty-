fcd7f3cfaa9c936915c562f3ce5d31ce841d2bb3
Terävä Olli <olli.s.terava@student.jyu.fi>
2022-10-24 14:29:57 +0300

    Korjattu bugeja, lisätty kommentteja,sekä timer

--- Harjoitustyö/KauppaPeli/KauppaPeli.cs
+++ Harjoitustyö/KauppaPeli/KauppaPeli.cs
@@ -12,54 +12,50 @@ public class KauppaPeli : PhysicsGame
     IntMeter pistelaskuri;
     DoubleMeter elamalaskuri;
     private EasyHighScore topkyba = new EasyHighScore();
-    List<Label> valikonKohdat;
+    private bool loppu = false;
 
     public override void Begin()
     {
-        Valikko();
+        ClearAll();
+        loppu = false;
 
-        BoundingRectangle alaosa = new BoundingRectangle(new Vector(Level.Left, 0), Level.BoundingRect.BottomRight);
-        BoundingRectangle ylaosa = new BoundingRectangle(Level.BoundingRect.TopLeft, new Vector(Level.Right, 0));
-        //TODO:BoundingRectangle kokopeli = new BoundingRectangle(Level.BoundingRect.BottomLeft, new Vector(Level.TopRight, 0)); ohjaus?'
+        BoundingRectangle koko = new BoundingRectangle(new Vector(Level.Left, - 200), Level.BoundingRect.TopRight);
         //TODO:Seinästä kimpoaminen objekteille vihaisetasiakkaat ja tuotteet
 
-        for (int i = 0; i < 15; i++) // vihaiset asiakkat
+        for (int i = 0; i < 10; i++) // vihaiset asiakkat
         {
-            PhysicsObject vihaisetAsiakkaat = LuoSatunaisetAsiakkaat(this, alaosa, 60, "vihu");
+            PhysicsObject vihaisetAsiakkaat = LuoSatunaisetAsiakkaat(this, koko, 30, "vihu");
             vihaisetAsiakkaat.Image = LoadImage("vihasiakas"); // creative commons kuva linkki https://fi.depositphotos.com/52847715/stock-photo-angry-customer-at-supermarket.html
                                                                // tekijä stokkete
 
 
         }
 
-        for (int i = 0; i < 20; i++) //kerättävät tuotteet
+        for (int i = 0; i < 20; i++) // kerättävät tuotteet
         {
-            PhysicsObject tuotteet = LuoTuotteet(this, alaosa, 60, "tuotteet");
+            PhysicsObject tuotteet = LuoTuotteet(this, koko, 0, "tuotteet");
             tuotteet.Image = LoadImage("tuotteet"); //Creative commons kuva linkki https://fi.depositphotos.com/47120155/stock-photo-shopping-basket-with-groceries.html
                                                     //Tekijä: chressiesjd
-
         }
 
-        PhysicsObject pelaaja = LuoSatunaisetAsiakkaat(this, ylaosa, 50, "pelaaja");
+        PhysicsObject pelaaja = LuoSatunaisetAsiakkaat(this, koko, 30, "pelaaja");
         pelaaja.Image = LoadImage("pelaaja");
 
-        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
         Level.Background.Image = LoadImage("background.jpeg"); //Cretive commons kuva linkki https://fi.depositphotos.com/10626948/stock-photo-supermarket.html
-                                                               //Tekijä: gyn9037
         Level.BackgroundColor = Color.White;
         Level.CreateBorders();
 
-        Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä avustus");
-        Keyboard.Listen(Key.Up, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja ylös", pelaaja, new Vector(0, 200));
-        Keyboard.Listen(Key.Down, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja alas", pelaaja, new Vector(0, -200));
-        Keyboard.Listen(Key.Left, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja vasemmalle", pelaaja, new Vector(-200, 0));
-        Keyboard.Listen(Key.Right, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja oikealle", pelaaja, new Vector(200, 0));
+        Timer.CreateAndStart(1.5, LuoTuote);
+        Timer.CreateAndStart(1.5, LuoAsiakas);
+
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
 
         AddCollisionHandler(pelaaja, "vihu", TormasiAsiakkaasen);
         AddCollisionHandler(pelaaja, "tuotteet", KerasiTuotteen);
         LuoPistelaskuri();
         LuoElamalaskuri();
+        LisaaOhjaimet(pelaaja);
     }
 
     /// <summary>
@@ -99,8 +95,8 @@ public class KauppaPeli : PhysicsGame
     /// <param name="tunniste">Tunniste tag</param>
     public static PhysicsObject LuoTuotteet(PhysicsGame peli, BoundingRectangle umpyra, double vauhti, string tunniste)
     {
-        double leveys = RandomGen.NextDouble(45, 45);
-        double korkeus = RandomGen.NextDouble(45, 45);
+        double leveys = RandomGen.NextDouble(50, 50);
+        double korkeus = RandomGen.NextDouble(50, 50);
         Vector liike = RandomGen.NextVector(0, vauhti);
         PhysicsObject tuotteet = new PhysicsObject(leveys, korkeus, Shape.Circle);
         tuotteet.Position = RandomGen.NextVector(umpyra);
@@ -109,6 +105,7 @@ public class KauppaPeli : PhysicsGame
         tuotteet.Hit(liike);
         peli.Add(tuotteet);
         return tuotteet;
+
     }
 
     /// <summary>
@@ -123,8 +120,14 @@ public class KauppaPeli : PhysicsGame
         rajahdys.UseShockWave = false;
         this.Add(rajahdys);
         elamalaskuri.Value -= 1;
+        MessageDisplay.Add("Varo vihaisia asiakkaita");
     }
 
+    /// <summary>
+    /// Tullaan, kun pelaaja kerää tuotteen
+    /// </summary>
+    /// <param name="pelaaja"> kuka kerää</param>
+    /// <param name="tuote"> minkä tuotteen </param>
     public void KerasiTuotteen(PhysicsObject pelaaja, PhysicsObject tuote)
     {
         tuote.Destroy();
@@ -132,6 +135,9 @@ public class KauppaPeli : PhysicsGame
         elamalaskuri.Value += 1;
     }
 
+    /// <summary>
+    /// Luo pelaajalle pistelaskurin
+    /// </summary>
     void LuoPistelaskuri()
     {
         pistelaskuri = new IntMeter(0);
@@ -139,86 +145,121 @@ public class KauppaPeli : PhysicsGame
         Label pistenaytto = new Label();
         pistenaytto.X = Screen.Left + 100;
         pistenaytto.Y = Screen.Top - 100;
-        pistenaytto.TextColor = Color.Black;
-        pistenaytto.Color = Color.White;
-        pistenaytto.Title = "Pisteesi  ";
-
+        pistenaytto.TextColor = Color.White;
+        pistenaytto.Color = Color.DarkBlue;
+        pistenaytto.Title = "Pisteesi ";
         pistenaytto.BindTo(pistelaskuri);
         Add(pistenaytto);
     }
 
-    void LuoElamalaskuri()
+    /// <summary>
+    /// Luo pelaajalle elamalaskurin
+    /// </summary>
+    private void LuoElamalaskuri()
     {
         elamalaskuri = new DoubleMeter(10);
-        elamalaskuri.MaxValue = 5;
-        elamalaskuri.LowerLimit += ElamaLoppui;
+        elamalaskuri.MaxValue = 4;
+        elamalaskuri.LowerLimit += Havio;
 
         ProgressBar elamapalkki = new ProgressBar(150, 20);
         elamapalkki.X = Screen.Right + 150;
         elamapalkki.Y = Screen.Top - 20;
         elamapalkki.BarColor = Color.Red;
         elamapalkki.BorderColor = Color.Black;
-        elamapalkki.Color = Color.White;
+        elamapalkki.Color = Color.DarkBlue;
+
         elamapalkki.BindTo(elamalaskuri);
         Add(elamapalkki);
     }
-    void ElamaLoppui()
+
+    /// <summary>
+    /// tapahtuu, kun pelaaja häviää
+    /// </summary>
+    private void Havio()
     {
+        if (loppu) return;
+        loppu = true;
         MessageDisplay.Add("Elämät loppuivat, peli päättyy");
         topkyba.EnterAndShow(pistelaskuri.Value);
-        //topkyba.HighScoreWindow.Closed += Valikko();
-    }
-    void Valikko()
-    {
-        Label otsikko = new Label("Pelin alkuvalikko");
-        otsikko.Y = 100;
-        otsikko.Font = new Font(40, true);
-        Add(otsikko); 
-
-        valikonKohdat = new List<Label>();
-        Label alku = new Label("Aloita uusi peli");
-        alku.Position = new Vector(0, 40);
-        valikonKohdat.Add(alku);
-
-        Label top = new Label("Parhaat pisteet");
-        top.Position = new Vector(0, 0);
-        valikonKohdat.Add(top);
-
-        Label loppu = new Label("Lopeta peli");
-        loppu.Position = new Vector(0, -40);
-        valikonKohdat.Add(loppu);
-
-        foreach (Label valikonKohta in valikonKohdat)
-        {
-            Add(valikonKohta);
-            valikonKohta.Color = Color.Aqua;
-        }
-
+        topkyba.HighScoreWindow.Closed += AloitaPeli;
         
-        Mouse.ListenOn(alku, MouseButton.Left, ButtonState.Pressed, AloitaPeli, null);
-        Mouse.ListenOn(top, MouseButton.Left, ButtonState.Pressed, Topkymppi, null);
-        Mouse.ListenOn(loppu, MouseButton.Left, ButtonState.Pressed, Exit, null);
     }
-    void Topkymppi()
+
+    /// <summary>
+    /// Annetaan pelaajalle vaihtoehtoja, eli mitä haluaa tehdä
+    /// </summary>
+    /// <param name="Sender">mistä tullaan</param>
+    private void AloitaPeli(Window Sender)
     {
-        foreach (Label valikonKohta in valikonKohdat)
+
+        string[] vaihtoehdot = { "Aloita peli", "Top kybä", "Lopeta peli" };
+        MultiSelectWindow alkuvalikko = new MultiSelectWindow("Alkuvalikko", vaihtoehdot);
+        alkuvalikko.ItemSelected += delegate (int valittu)
         {
-            Remove(valikonKohta);
-        }
+            if (valittu == 0) Begin();
+            if (valittu == 1) TopKymppi();
+            if (valittu == 2) Exit();
+        };
+        Add(alkuvalikko);
+
+    }
+
+    /// <summary>
+    /// Tullaan, kun valitaan vaihtoehdoista TOPKYMPPI
+    /// </summary>
+    private void TopKymppi()
+    {
         topkyba.Show();
-        //topkyba.HighScoreWindow.Closed += Valikko();
-
+        topkyba.HighScoreWindow.Closed += AloitaPeli;
     }
-    void AloitaPeli()
+
+    /// <summary>
+    /// Luo timerin avulla luotteet
+    /// </summary>
+    private void LuoTuote()
     {
-        foreach (Label valikonKohta in valikonKohdat)
-        {
-            Remove(valikonKohta);
-        }
+        double leveys = RandomGen.NextDouble(50, 50);
+        double korkeus = RandomGen.NextDouble(50, 50);
+        PhysicsObject tuotteet = new PhysicsObject(leveys, korkeus, Shape.Circle);
+        tuotteet.Position = RandomGen.NextVector(Level.Bottom,Level.Top) ;
+        tuotteet.Tag = "tuotteet";
+        tuotteet.Image = LoadImage("tuotteet");
+        tuotteet.LifetimeLeft = TimeSpan.FromSeconds(7);
+        Vector liike = RandomGen.NextVector(0, 0);
+        tuotteet.Hit(liike);
+        Add(tuotteet);
+    }
+
+    /// <summary>
+    /// Luo timerin avulla peliin asiakkaita
+    /// </summary>
+    private void LuoAsiakas()
+    {
+        double leveys = RandomGen.NextDouble(50, 50);
+        double korkeus = RandomGen.NextDouble(50, 50);
+        PhysicsObject asiakkaat = new PhysicsObject(leveys, korkeus, Shape.Circle);
+        asiakkaat.Position = RandomGen.NextVector(Level.Bottom,Level.Top);
+        asiakkaat.Tag = "vihu";
+        asiakkaat.Image = LoadImage("vihasiakas");
+        asiakkaat.LifetimeLeft = TimeSpan.FromSeconds(5);
+        Vector liike = RandomGen.NextVector(0, 30);
+        asiakkaat.Hit(liike);
+        Add(asiakkaat);
+    }
+
+    /// <summary>
+    /// Lisää peliin ohjaimet
+    /// </summary>
+    /// <param name="kenelle">kenelle lisätään</param>
+    private void LisaaOhjaimet(PhysicsObject kenelle)
+    {
+        Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä avustus");
+        Keyboard.Listen(Key.Up, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja ylös", kenelle, new Vector(0, 200));
+        Keyboard.Listen(Key.Down, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja alas", kenelle, new Vector(0, -200));
+        Keyboard.Listen(Key.Left, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja vasemmalle", kenelle, new Vector(-200, 0));
+        Keyboard.Listen(Key.Right, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja oikealle", kenelle, new Vector(200, 0));
 
     }
-    
-
 }
 
 
