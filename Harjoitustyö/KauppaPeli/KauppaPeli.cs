01fd484eaa3bf104ef22d785f1fad33d69796b47
Terävä Olli <olli.s.terava@student.jyu.fi>
2022-11-04 14:59:22 +0200

    100% suoritus

--- Harjoitustyö/KauppaPeli/KauppaPeli.cs
+++ Harjoitustyö/KauppaPeli/KauppaPeli.cs
@@ -7,6 +7,11 @@ using Jypeli.Widgets;
 
 namespace KauppaPeli;
 
+/// @author osterava
+/// @version 4.11.2022
+/// <summary>
+///  Peli, jossa keräillään tuotteita omaan ostoskärryyn
+/// </summary>
 public class KauppaPeli : PhysicsGame
 {
     IntMeter pistelaskuri;
@@ -22,123 +27,97 @@ public class KauppaPeli : PhysicsGame
         BoundingRectangle koko = new BoundingRectangle(new Vector(Level.Left, - 200), Level.BoundingRect.TopRight);
         //TODO:Seinästä kimpoaminen objekteille vihaisetasiakkaat ja tuotteet
 
-        for (int i = 0; i < 10; i++) // vihaiset asiakkat
+        for (int i = 0; i < 10; i++) // juomat
         {
-            PhysicsObject vihaisetAsiakkaat = LuoSatunaisetAsiakkaat(this, koko, 30, "vihu");
-            vihaisetAsiakkaat.Image = LoadImage("vihasiakas"); // creative commons kuva linkki https://fi.depositphotos.com/52847715/stock-photo-angry-customer-at-supermarket.html
+            PhysicsObject juomat = LuoJuomat(this, koko, 30, "tuotteet");
+            juomat.Image = LoadImage("juoma.png"); // creative commons kuva linkki https://fi.depositphotos.com/52847715/stock-photo-angry-customer-at-supermarket.html
                                                                // tekijä stokkete
-
-
         }
 
-        for (int i = 0; i < 20; i++) // kerättävät tuotteet
-        {
-            PhysicsObject tuotteet = LuoTuotteet(this, koko, 0, "tuotteet");
-            tuotteet.Image = LoadImage("tuotteet"); //Creative commons kuva linkki https://fi.depositphotos.com/47120155/stock-photo-shopping-basket-with-groceries.html
-                                                    //Tekijä: chressiesjd
-        }
 
-        PhysicsObject pelaaja = LuoSatunaisetAsiakkaat(this, koko, 30, "pelaaja");
+        PhysicsObject pelaaja = LuoJuomat(this, koko, 30, "pelaaja");
         pelaaja.Image = LoadImage("pelaaja");
 
-        Level.Background.Image = LoadImage("background.jpeg"); //Cretive commons kuva linkki https://fi.depositphotos.com/10626948/stock-photo-supermarket.html
+        Level.Background.Image = LoadImage("background2.jpeg"); //Cretive commons kuva linkki https://fi.depositphotos.com/10626948/stock-photo-supermarket.html
         Level.BackgroundColor = Color.White;
         Level.CreateBorders();
 
-        Timer.CreateAndStart(1.5, LuoTuote);
+        Timer.CreateAndStart(1, LuoTuote);
         Timer.CreateAndStart(1.5, LuoAsiakas);
-
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
-        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
 
         AddCollisionHandler(pelaaja, "vihu", TormasiAsiakkaasen);
         AddCollisionHandler(pelaaja, "tuotteet", KerasiTuotteen);
+
         LuoPistelaskuri();
         LuoElamalaskuri();
         LisaaOhjaimet(pelaaja);
+
     }
 
+
     /// <summary>
-    /// Aliohjelma luo vihamieliset asiakkaat
+    ///     Luo pelaajalle kerättäviä juomia
     /// </summary>
     /// <param name="peli">mihin peliin ympyrä luodaan</param>
     /// <param name="umpyra">minkä ymppyrän sisälle luodaan eli paikka</param>
     /// <param name="vauhti">ympyrän vauhti alussa</param>
     /// <param name="tunniste">Tunniste tag</param>
-    public static PhysicsObject LuoSatunaisetAsiakkaat(PhysicsGame peli, BoundingRectangle umpyra, double vauhti, string tunniste)
+    private static PhysicsObject LuoJuomat(PhysicsGame peli, BoundingRectangle umpyra, double vauhti, string tunniste)
     {
         double leveys = RandomGen.NextDouble(50, 50);
         double korkeus = RandomGen.NextDouble(50, 50);
-        Vector liike = RandomGen.NextVector(0, vauhti);
-        PhysicsObject asiakkaat = new PhysicsObject(leveys, korkeus, Shape.Circle);
-        asiakkaat.Position = RandomGen.NextVector(umpyra);
-        asiakkaat.Hit(liike);
-        asiakkaat.Tag = "vihu";
-        peli.Add(asiakkaat);
-        return asiakkaat;
+        PhysicsObject juoma = new PhysicsObject(leveys, korkeus, Shape.Circle);
+        juoma.Position = RandomGen.NextVector(umpyra);
+        juoma.Tag = "tuotteet";
+        peli.Add(juoma);
+        return juoma;
     }
+
+
     /// <summary>
-    /// lyödään kolmiota eli pelaajaa voimavektorilla 
+    ///     Lyödään ympyrää eli pelaajaa voimavektorilla 
     /// </summary>
     /// <param name="ympyra">lyötävä kohde</param>
     /// <param name="mihin">voimavektori</param>
-    public static void LiikutaPelaajaa(PhysicsObject ympyra, Vector mihin)
+    private static void LiikutaPelaajaa(PhysicsObject ympyra, Vector mihin)
     {
         ympyra.Hit(mihin);
     }
-    /// <summary>
-    /// Aliohjelma luo vihamieliset asiakkaat
-    /// </summary>
-    /// <param name="peli">mihin peliin ympyrä luodaan</param>
-    /// <param name="umpyra">minkä ymppyrän sisälle luodaan eli paikka</param>
-    /// <param name="vauhti">ympyrän vauhti alussa</param>
-    /// <param name="tunniste">Tunniste tag</param>
-    public static PhysicsObject LuoTuotteet(PhysicsGame peli, BoundingRectangle umpyra, double vauhti, string tunniste)
-    {
-        double leveys = RandomGen.NextDouble(50, 50);
-        double korkeus = RandomGen.NextDouble(50, 50);
-        Vector liike = RandomGen.NextVector(0, vauhti);
-        PhysicsObject tuotteet = new PhysicsObject(leveys, korkeus, Shape.Circle);
-        tuotteet.Position = RandomGen.NextVector(umpyra);
-        tuotteet.Color = Color.Blue;
-        tuotteet.Tag = "tuotteet";
-        tuotteet.Hit(liike);
-        peli.Add(tuotteet);
-        return tuotteet;
 
-    }
 
     /// <summary>
-    /// Metodi, johon tullan, kun pelaaja osuu johonkin vihaiseen asiakkaaseen
+    ///     Metodi, johon tullan, kun pelaaja osuu johonkin vihaiseen asiakkaaseen
     /// </summary>
     /// <param name="pelaaja"></param>
     /// <param name="vihu"></param>
-    public void TormasiAsiakkaasen(PhysicsObject pelaaja, PhysicsObject vihu)
+    private void TormasiAsiakkaasen(PhysicsObject pelaaja, PhysicsObject vihu)
     {
         Explosion rajahdys = new Explosion(vihu.Width * 2);
         rajahdys.Position = vihu.Position;
         rajahdys.UseShockWave = false;
         this.Add(rajahdys);
         elamalaskuri.Value -= 1;
-        MessageDisplay.Add("Varo vihaisia asiakkaita");
+        MessageDisplay.Add("VARO VIHAISIA ASIAKKAITA");
     }
 
+
     /// <summary>
-    /// Tullaan, kun pelaaja kerää tuotteen
+    ///     Metodi,johon tullaan, kun pelaaja kerää tuotteen
     /// </summary>
     /// <param name="pelaaja"> kuka kerää</param>
     /// <param name="tuote"> minkä tuotteen </param>
-    public void KerasiTuotteen(PhysicsObject pelaaja, PhysicsObject tuote)
+    private void KerasiTuotteen(PhysicsObject pelaaja, PhysicsObject tuote)
     {
         tuote.Destroy();
         pistelaskuri.Value += 1;
         elamalaskuri.Value += 1;
     }
 
+
     /// <summary>
-    /// Luo pelaajalle pistelaskurin
+    ///     Luo pelaajalle pistelaskurin
     /// </summary>
-    void LuoPistelaskuri()
+    private void LuoPistelaskuri()
     {
         pistelaskuri = new IntMeter(0);
 
@@ -146,34 +125,36 @@ public class KauppaPeli : PhysicsGame
         pistenaytto.X = Screen.Left + 100;
         pistenaytto.Y = Screen.Top - 100;
         pistenaytto.TextColor = Color.White;
-        pistenaytto.Color = Color.DarkBlue;
-        pistenaytto.Title = "Pisteesi ";
+        pistenaytto.Color = Color.Black;
+        pistenaytto.Title = " Pisteet ";
         pistenaytto.BindTo(pistelaskuri);
         Add(pistenaytto);
     }
 
+
     /// <summary>
-    /// Luo pelaajalle elamalaskurin
+    ///     Luo pelaajalle elamalaskurin
     /// </summary>
     private void LuoElamalaskuri()
     {
-        elamalaskuri = new DoubleMeter(10);
-        elamalaskuri.MaxValue = 4;
+        elamalaskuri = new DoubleMeter(5);
+        elamalaskuri.MaxValue = 5;
         elamalaskuri.LowerLimit += Havio;
 
-        ProgressBar elamapalkki = new ProgressBar(150, 20);
-        elamapalkki.X = Screen.Right + 150;
-        elamapalkki.Y = Screen.Top - 20;
+        ProgressBar elamapalkki = new ProgressBar(150,30);
+        elamapalkki.X = Level.Right - 100;
+        elamapalkki.Y = Level.Top - 100;
         elamapalkki.BarColor = Color.Red;
         elamapalkki.BorderColor = Color.Black;
-        elamapalkki.Color = Color.DarkBlue;
+        elamapalkki.Color = Color.Black;
 
         elamapalkki.BindTo(elamalaskuri);
         Add(elamapalkki);
     }
 
+
     /// <summary>
-    /// tapahtuu, kun pelaaja häviää
+    ///     Metodi,tapahtuu, kun pelaaja häviää
     /// </summary>
     private void Havio()
     {
@@ -184,6 +165,7 @@ public class KauppaPeli : PhysicsGame
         topkyba.HighScoreWindow.Closed += AloitaPeli;
         
     }
+
 
     /// <summary>
     /// Annetaan pelaajalle vaihtoehtoja, eli mitä haluaa tehdä
@@ -204,6 +186,7 @@ public class KauppaPeli : PhysicsGame
 
     }
 
+
     /// <summary>
     /// Tullaan, kun valitaan vaihtoehdoista TOPKYMPPI
     /// </summary>
@@ -223,15 +206,16 @@ public class KauppaPeli : PhysicsGame
         PhysicsObject tuotteet = new PhysicsObject(leveys, korkeus, Shape.Circle);
         tuotteet.Position = RandomGen.NextVector(Level.Bottom,Level.Top) ;
         tuotteet.Tag = "tuotteet";
-        tuotteet.Image = LoadImage("tuotteet");
-        tuotteet.LifetimeLeft = TimeSpan.FromSeconds(7);
+        tuotteet.Image = LoadImage("tuotteet.ong.png");
+        tuotteet.LifetimeLeft = TimeSpan.FromSeconds(9);
         Vector liike = RandomGen.NextVector(0, 0);
         tuotteet.Hit(liike);
         Add(tuotteet);
     }
 
+
     /// <summary>
-    /// Luo timerin avulla peliin asiakkaita
+    ///     Luo timerin avulla peliin asiakkaita
     /// </summary>
     private void LuoAsiakas()
     {
@@ -240,19 +224,21 @@ public class KauppaPeli : PhysicsGame
         PhysicsObject asiakkaat = new PhysicsObject(leveys, korkeus, Shape.Circle);
         asiakkaat.Position = RandomGen.NextVector(Level.Bottom,Level.Top);
         asiakkaat.Tag = "vihu";
-        asiakkaat.Image = LoadImage("vihasiakas");
+        asiakkaat.Image = LoadImage("asiakas");
         asiakkaat.LifetimeLeft = TimeSpan.FromSeconds(5);
         Vector liike = RandomGen.NextVector(0, 30);
         asiakkaat.Hit(liike);
         Add(asiakkaat);
     }
 
+
     /// <summary>
-    /// Lisää peliin ohjaimet
+    ///     Lisää peliin ohjaimet
     /// </summary>
     /// <param name="kenelle">kenelle lisätään</param>
     private void LisaaOhjaimet(PhysicsObject kenelle)
     {
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä avustus");
         Keyboard.Listen(Key.Up, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja ylös", kenelle, new Vector(0, 200));
         Keyboard.Listen(Key.Down, ButtonState.Pressed, LiikutaPelaajaa, "Pelaaja alas", kenelle, new Vector(0, -200));
